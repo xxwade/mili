00a45cc8561f0c3339cb506b8e614687a495d2c0
许鑫 <xin.xu1@yitu-inc.com>
2020-06-23 16:49:09 +0800

    feat: mili-app

---
+++ packages/mili-lint/src/index.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+const eslint = require("./eslint");
+const stylelint = require("./stylelint");
+const prettier = require("./prettier");
+
+module.exports = {
+  stylelint,
+  prettier,
+  eslint,
+  default: eslint,
+};
